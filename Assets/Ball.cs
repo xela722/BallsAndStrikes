5b8cbe8f4a231ed66b012e249ce078f9aa7d5df1
xela722 <alex0722@comcast.net>
2019-03-28 18:41:49 -0400

    Add Random Pitch Movement

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    public float speed = 1.0f;
+    public BoxCollider2D StrikeZone;
+
+    Vector3 pitchPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+        pitchPosition = randomPitchLocation();
 	}
 	
+    Vector3 randomPitchLocation()
+    {
+        return new Vector3(
+            Random.Range(StrikeZone.bounds.min.x, StrikeZone.bounds.max.x),
+            Random.Range(StrikeZone.bounds.min.y, StrikeZone.bounds.max.y)
+            );
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, pitchPosition, step);	
 	}
 }
