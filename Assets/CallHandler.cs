f7b55f42d86d64d0dfc13534e499b1a703138878
xela722 <alex0722@comcast.net>
2019-04-11 22:42:12 -0400

    Add UI and basic CallHandling

---
+++ Assets/CallHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallHandler : MonoBehaviour {
+
+    public Ball theBall;
+
+    public void callBall()
+    {
+        if (theBall.isStrike())
+        {
+            Debug.Log("WRONG");
+            theBall.newPitch();
+        }
+    }
+    public void callStrike()
+    {
+        if (theBall.isStrike())
+        {
+            Debug.Log("CORRECT");
+            theBall.newPitch();
+        }
+    }
+}
