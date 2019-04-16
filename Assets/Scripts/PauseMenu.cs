56cf4c9db430290082d48cc4d6f58fd344a76b2c
xela722 <alex0722@comcast.net>
2019-04-16 10:14:26 -0400

    Add Basic Main and Pause Menu Functionality

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
