312849b070c37b4539df02bca7c3c2bf815e9410
xela722 <alex0722@comcast.net>
2019-04-16 17:54:42 -0400

    Add Ball Strike Out Inning Counter Functionality

--- Assets/Scripts/CallHandler.cs
+++ Assets/Scripts/CallHandler.cs
@@ -10,26 +10,59 @@ public class CallHandler : MonoBehaviour {
     public Text ballText;
     public Text strikeText;
     public Text outText;
+    public Text inningText;
 
     public int balls, strikes, outs = 0;
+    public int inning = 1;
 
     public void callBall()
     {
-        if (theBall.isStrike())
-        {
-            theBall.newPitch();
-        }
-        balls += 1;
-        ballText.text = string.Format("Balls: {0}", balls);
+        theBall.newPitch();
+        updateBalls(balls + 1);
     }
     public void callStrike()
     {
-        if (theBall.isStrike())
+        theBall.newPitch();
+        updateStrikes(strikes + 1);
+
+        if (strikes == 3)
         {
-            
-            theBall.newPitch();
+            updateStrikes(0);
+            updateOuts(outs + 1);
+            if(outs == 3)
+            {
+                nextInning();
+            }
         }
-        strikes += 1;
+    }
+
+    void updateBalls(int ballCount)
+    {
+        balls = ballCount;
+        ballText.text = string.Format("Balls: {0}", balls);
+    }
+    void updateStrikes(int strikeCount)
+    {
+        strikes = strikeCount;
         strikeText.text = string.Format("Strikes: {0}", strikes);
+    }
+    void updateOuts(int outCount)
+    {
+        outs = outCount;
+        outText.text = string.Format("Outs: {0}", outs);
+    }
+    void updateInning(int inningCount)
+    {
+        inning = inningCount;
+        inningText.text = string.Format("Inning: {0}", inning);
+    }
+
+    public void nextInning()
+    {
+        updateInning(inning + 1);
+
+        updateBalls(0);
+        updateStrikes(0);
+        updateOuts(0);
     }
 }
